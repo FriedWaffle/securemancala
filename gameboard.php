e8dd9a0ae0b1fc5939975fdb03e7023296eb3c35
Vanguard714 <Joel.parker714@outlook.com>
2020-11-29 05:24:43 +0000

    improved game

--- gameboard.php
+++ gameboard.php
@@ -29,6 +29,11 @@
                 fill:#8B5E13;
             }
 
+            .rect{fill:none;}
+	        .st3{font-family:'MyriadPro-Regular';}
+	        .st4{font-size:500px;}
+	        .st5{font-size:300px;}
+
             .goal{
                 fill:#8B5E13;
             }
@@ -49,6 +54,8 @@
 
             var gameBoard = (function()
             {
+                var leftScore = 0;
+                var rightScore = 0;
                 var svgns = "http://www.w3.org/2000/svg";
                 function randomize(min, max){
                     max -= min;
@@ -82,6 +89,18 @@
                 }
 
                 
+                function turn(x)
+                {
+                    if(x == 'first')
+                    {
+                        return 'second';
+                    }
+                    else
+                    {
+                        return 'first';
+                    }
+                }
+                
 
                 function setupClick(list)
                 {
@@ -89,42 +108,50 @@
                     {
                         console.log(e.path[0].id);
                        
-                        var currentIndex = e.path[0].id;
+                        let currentIndex = e.path[0].id;
+                        console.log(currentIndex);
                         var test = $$$('slot')[e.path[0]];
                         var num = $$$('slot')[e.path[0].id].childNodes.length;
-                        console.log(num);
+                        //console.log(num);
+                        
                         $$$('slot')[e.path[0].id].innerHTML = '';
 
-
-                        if($$$('slot')[e.path[0].id].childNodes.length == 0)
-                        {
                             for(let i = 1; i <= num; i++)
                             {   
-                                if(currentIndex < 11)
-                                {
-                                    currentIndex++;
-                                }
-                                else if(currentIndex == 11)
-                                else
-                                {
-                                    currentIndex = 0;
-                                }                         
-                                
-                                
-                                makeCircle($(currentIndex).cx.baseVal.value, $(currentIndex).cy.baseVal.value,currentIndex);
-                            }
-                        }
-                        
+                               //console.log(currentIndex);
+                               if(currentIndex == 5)
+                               {
+                                   currentIndex = 11;
+                                   makeCircle($(currentIndex).cx.baseVal.value, $(currentIndex).cy.baseVal.value,currentIndex);
+                                   rightScore++;
+                                   $('rScore').innerHTML = `${rightScore}`;
+                                   console.log('Right Score: '+rightScore);
+                               }
+                               if(currentIndex == 6)
+                               {
+                                   currentIndex = 0;
+                                   makeCircle($(currentIndex).cx.baseVal.value, $(currentIndex).cy.baseVal.value,currentIndex);
+                                   leftScore++;
+                                   $('lScore').innerHTML = `${leftScore}`;
+                                   console.log('Left Score: '+leftScore);
+                               }
+                               if(currentIndex < 5)
+                               {
+                                   currentIndex++;
+                                   makeCircle($(currentIndex).cx.baseVal.value, $(currentIndex).cy.baseVal.value,currentIndex);
+                               }
+                               else if(currentIndex > 6)
+                               {
+                                   currentIndex--;
+                                   makeCircle($(currentIndex).cx.baseVal.value, $(currentIndex).cy.baseVal.value,currentIndex);
+                               }
+                            }           
                     }
-                }
-
-                function moveStones()
-                {
-
                 }
                 
                 function init()
                 {
+                    
 
                     var getClasses = document.getElementsByClassName('st1');
     
@@ -179,7 +206,6 @@
 
 		<ellipse  class="st1" cx="3543.47" cy="1605.48" rx="224.09" ry="223.93" />
 
-
 		<ellipse  class="st1" cx="1009.42" cy="797.48" rx="224.09" ry="223.93"/>
 
         
@@ -196,9 +222,10 @@
 
 		<ellipse  class="st1" cx="3548.47" cy="796.48" rx="224.09" ry="223.93" />
 
-	<path class="goal" d="M642.85,1826H218.15c-56.97,0-103.15-46.18-103.15-103.15V673.15C115,616.18,161.18,570,218.15,570h424.69
-		C699.82,570,746,616.18,746,673.15v1049.69C746,1779.82,699.82,1826,642.85,1826z"/>
-	<path class="goal" d="M4355.85,1857h-424.69c-56.97,0-103.15-46.18-103.15-103.15V704.15c0-56.97,46.18-103.15,103.15-103.15h424.69
+	<path class="left goal" d="M642.85,1826H218.15c-56.97,0-103.15-46.18-103.15-103.15V673.15C115,616.18,161.18,570,218.15,570h424.69
+        C699.82,570,746,616.18,746,673.15v1049.69C746,1779.82,699.82,1826,642.85,1826z"/>
+        
+	<path class="right goal" d="M4355.85,1857h-424.69c-56.97,0-103.15-46.18-103.15-103.15V704.15c0-56.97,46.18-103.15,103.15-103.15h424.69
 		c56.97,0,103.15,46.18,103.15,103.15v1049.69C4459,1810.82,4412.82,1857,4355.85,1857z"/>
 <g id="marbles">
     <g class='slot'></g>
@@ -248,6 +275,14 @@
 
 		<ellipse id='11'  class="st2 right one" cx="3548.47" cy="796.48" rx="224.09" ry="223.93" />
     </g>
+
+    <rect x="292.44" y="992.04" class="rect" width="276.56" height="411.96"/>
+    <text transform="matrix(1 0 0 1 292.4448 1347.0327)" id="lScore" class="st3 st4">0</text>
+    <rect x="4005.44" y="1018.04" class="rect" width="276.56" height="411.96"/>
+    <text transform="matrix(1 0 0 1 4005.4448 1373.0327)" id="rScore" class="st3 st4">0</text>
+    <rect x="1465.19" y="136" class="rect" width="1884.12" height="434.58"/>
+    <text transform="matrix(1 0 0 1 1465.1924 348.9976)" class="st3 st5">Player Two</text>
+    <text transform="matrix(1 0 0 1 1465.4946 2269.5615)" class="st3 st5">Player One</text>
 
 </svg>
     </body>
